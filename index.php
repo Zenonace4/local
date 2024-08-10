fbb28887ffcdc86ea4b13e98196cd7bb31745738
Zenonace4 <anthonyjecaat@gmail.com>
2024-08-10 17:54:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 include "db.php";
-$usu1 = $_POST['usuario1'];
-$pass1 = $_POST['clave1'];
+//$usu1 = $_POST['usuario1'];
+//$pass1 = $_POST['clave1'];
 //$usu1 = $_REQUEST['usuario1'];
 //$pass1 = $_REQUEST['clave1'];
-//$usu1 = "valor";
-//$pass1 = "valor";
+$usu1 = "a";
+$pass1 = "a";
 if ($usu1 == "" || $pass1 == "") {
     echo "ERROR1";
 } else {
     $db = new Database();
     $con = $db->conectar();
-    $sql = $con->prepare("SELECT * FROM Registro where nombre=? AND valor=?");
+    $sql = $con->prepare("SELECT * FROM Registro where usuarioe=? AND contra=?");
     $sql->bindParam(1, $usu1);
     $sql->bindParam(2, $pass1);
     $sql->execute();
